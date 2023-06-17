40d737ad8894174567bb419696fc3af82632b922
Szymon Miękus <miekusoft@gmail.com>
2023-06-17 23:09:49 +0200

    Update text info

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -24,14 +24,25 @@ struct HomeView: View {
                 .frame(maxHeight: .infinity, alignment: .top)
                 .padding(.top, 257)
             
-            VStack {
+            // Mark: Current Weather
+            VStack(spacing: -10) {
                 Text("Montreal")
                     .font(.largeTitle)
                 
                 VStack {
-                    Text("19°C" + "\n" + "Mostlt Clear")
+//                    Text("19°C")
+//                        .font(.system(size: 96, weight: .thin))
+//                        .foregroundColor(.primary)
+//                    +
+//                    Text("\n")
+//                    +
+//                    Text("Mostly Clear")
+//                        .font(.title3.weight(.semibold))
+//                        .foregroundColor(.secondary)
                     
-                    Text("H:24°C  L:18°C")
+                    Text(attributedString)
+                    
+                    Text("H:24°C L:18°C")
                         .font(.title3.weight(.semibold))
                     
                     Spacer()
@@ -39,6 +50,27 @@ struct HomeView: View {
             }
             .padding(.top, 51)
         }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        
+        return string;
     }
 }
 

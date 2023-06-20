c609e8ca587690e7a0a2090ddd5c3796a53d3447
Szymon Miękus <miekusoft@gmail.com>
2023-06-20 13:01:37 +0200

    Weather widget

--- Weather/Views/Components/WeatherWidget.swift
+++ Weather/Views/Components/WeatherWidget.swift
@@ -8,13 +8,56 @@
 import SwiftUI
 
 struct WeatherWidget: View {
+    var forecast: Forecast
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            // MarkL Trapezoid
+            Trapezoid()
+                .fill(Color.weatherWidgetBackground)
+                .frame(width: 342, height: 174)
+            
+            //Mark: Content
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading, spacing: 8) {
+                    // Mark: Forecast temperature
+                    Text("\(forecast.temperature)°")
+                        .font(.system(size: 64))
+                    
+                    VStack(alignment: .leading, spacing: 2) {
+                        // Mark: Forecast temperature range
+                        Text("H:\(forecast.high)° L:\(forecast.low)°")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        
+                        // Mark: Forecast location
+                        Text(forecast.location)
+                            .font(.body)
+                            .lineLimit(1)
+                    }
+                }
+                
+                VStack(alignment: .trailing, spacing: 0) {
+                    // Mark: Forecast large icon
+                    Image("\(forecast.icon) large")
+                        .padding(.trailing, 4)
+                    
+                    // Mark: Weather
+                    Text(forecast.weather.rawValue)
+                        .font(.footnote)
+                        .padding(.trailing, 24)
+                }
+            }
+            .foregroundColor(.white)
+            .padding(.bottom, 20)
+            .padding(.leading, 20)
+        }
+        .frame(width: 342, height: 184, alignment: .bottom)
     }
 }
 
 struct WeatherWidget_Previews: PreviewProvider {
     static var previews: some View {
-        WeatherWidget()
+        WeatherWidget(forecast: Forecast.cities[0])
+            .preferredColorScheme(.dark)
     }
 }
--- Weather/Views/Detail/WeatherView.swift
+++ Weather/Views/Detail/WeatherView.swift
@@ -12,6 +12,19 @@ struct WeatherView: View {
         ZStack {
             // Mark: background
             Color.background.ignoresSafeArea()
+            
+            // Mark: Weather widgets
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(Forecast.cities) { forecast in
+                        WeatherWidget(forecast: forecast)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                EmptyView()
+                    .frame(height: 110)
+            }
         }
         .overlay{
             NavigationBar()
